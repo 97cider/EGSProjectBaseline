b39af75d9d3e5c34c1f86c7e1c499c830f894354
Youkilledkenny <smcfadden150@gmail.com>
2018-01-18 19:35:44 -0500

    Redid art to represent the clean slate, start of camera follow

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    //will need to be refactored to include player instances
+    public Transform target;
+    //so the player stays in the lower portion of the screen
+    public float heightOffset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (target != null)
+        {
+            //still needs a lot of work tee bee hahce
+            //it still follows the player at any y change, which makes the camera look really wierd while jumping 
+            //there needs to be a minimum change in y distance for a vertical change to occur
+            //for example there should be instances where the character is at the top of the screen, and some where he is at the botton
+            //top of a cliff, bottom of a pit, etc
+            //maximize screenspace in relevancy to the direction the player must be going (ie artificially direct the player where to go by screen size)
+            transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y + heightOffset, -10), 0.1f);
+        }
+    }
+}

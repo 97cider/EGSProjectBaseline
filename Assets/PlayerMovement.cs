1a192c228dec890453f6a374b78b10a3f8112581
Youkilledkenny <smcfadden150@gmail.com>
2018-01-19 01:01:09 -0500

    Started working on movement system using unified defined player states

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Animator AnimatorComponent;
+    public float movementSpeed, jumpHeight, horizontalMult;
+    public bool isJumping;
+    // Use this for initialization
+    void Start()
+    {
+        //UNITY WHAT THE FUCK
+        //alright, well we know that the player will start is a specific spot for now, we will have to change this when saving/loading is introduced
+        //oh god i hope this is serializable
+        horizontalMult = 0;
+        PlayerStates.Instance.Horizontal = Horizontal.idle;
+        PlayerStates.Instance.Vertical = Vertical.InAir;
+        PlayerStates.Instance.DirectionFacing = DirectionFacing.Right;
+        PlayerStates.Instance.Action = Action.Idle;
+    }
+
+    void FixedUpdate()
+    {
+        //handle walking
+        //handle jumping 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/PlayerStates.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStates : MonoBehaviour {
+    //a class used to store a collection of states used by the player
+    //hopefully this makes it more of an instanced scenario, which
+    //might make Co-Op easier down the road
+
+    //I HOPE PLEASE MAKE CO OP EASY I DONT WANT TO REDO THIS AGAIn
+    // Use this for initialization
+    private static PlayerStates _instanced;
+    public static PlayerStates Instance
+    {
+        get
+        {
+            if (_instanced == null)
+            {
+                _instanced = new GameObject("shits broken yo").AddComponent<PlayerStates>();
+            }
+            return _instanced;
+        }
+    }
+    //State storage
+    //im guessing an enum is the best way to go
+    //based on basic 2D movement systems, it would be great to help tie 
+    //the action system here as well
+    public Horizontal Horizontal;
+    public Vertical Vertical;
+    public DirectionFacing DirectionFacing;
+    public Action Action;
+}
+    public enum Horizontal
+    {
+        idle = 0,
+        mRight = 1,
+        mLeft = -1
+    }
+    //states for the players potential vetical movement
+    //climbing and hooked should be the same, but whatever
+    public enum Vertical
+    {
+        OnGround,
+        InAir,
+        Climbing,
+        Hooked
+    }
+    //basic direction facing
+    public enum DirectionFacing
+    {
+        Right = 1,
+        Left = -1
+    }
+    //i wanted to make one state for attacking, but it seems like
+    //we can do some cool stuff with light and heavy. For example,
+    //multiple attacks per weapon are confirmed, but maybe to keep the 
+    //game balanced, a heavy attack can stop all momentum of the player
+
+    //interacting is prolly just going to be looting and gathering materials
+    public enum Action
+    {
+        Idle,
+        LightAttack,
+        HeavyAttack,
+        Interacting,
+        Dialouge
+    }

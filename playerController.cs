e65c99c808c8304db6d959018c193d7aea133112
Brendan McFadden <bmcfadden150@gmail.com>
2018-01-30 09:26:13 +0800

    First Iteration of networking
    Changes the character controller, hence the separate branch

---
+++ playerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerController : Monobehavior
+{
+    void Update()
+    {
+        var xMove = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var yMove = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, xMove, 0);
+        transform.Translate(0, 0, yMove);
+    }
+}

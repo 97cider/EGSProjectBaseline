ff67a2d9c872ce8096bd4f75170dda7ebc5b1f58
Brendan McFadden <bmcfadden150@gmail.com>
2018-01-31 09:32:07 +0800

    Co-op working
    Coop and bmm349's character controller in and working

---
+++ Assets/TestScripts/TestAssets/nonRigiMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class nonRigiMove : NetworkBehaviour
+{
+    public void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        transform.Translate(x, 0, 0);
+
+    }
+}

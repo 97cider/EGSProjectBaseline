4d23ce716c69bd2c5c59b501a97d65e6e01fc1da
Youkilledkenny <smcfadden150@gmail.com>
2018-01-31 23:25:07 -0500

    added parallax support

---
+++ Assets/ParallaxBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour {
+    public float scrollSpeed;
+    Camera mainCamera;
+    Vector2 parallaxFollowCamera;
+    private float offset;
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main;
+        parallaxFollowCamera = transform.position;
+        offset = transform.position.x;
+    }
+	
+	// idk if this will make performance easier down the road but whatever 
+	void LateUpdate () {
+        parallaxFollowCamera.x = mainCamera.transform.position.x * scrollSpeed + offset;
+        transform.position = parallaxFollowCamera;
+    }
+}

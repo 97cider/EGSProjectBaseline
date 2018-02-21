70195e3892a64d6fface8a0dbe5b1e42ff4c5b5d
bmm349 <bmm349@drexel.edu>
2018-02-20 20:24:57 -0500

    infinite loop commit

--- Assets/UI/UIMovement.cs
+++ Assets/UI/UIMovement.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UIMovement : MonoBehaviour {
+    //Really basic camera movement script to speed up UI (aka make me do less)
+    public Camera sceneCam;
+    public Transform MainCameraPosition;
+    public Transform DesiredScenePosition;
+    public float flySpeed = 1.0F;
+    private float beginTime;
+    private float lengthToTravel;
+    public Vector3 i;
+    public void Start() {
+        beginTime = Time.time;
+        lengthToTravel = Vector3.Distance(MainCameraPosition.position, DesiredScenePosition.position);
+    }
+    public void cameraMovement()
+    {
+        Transform currentLocation = MainCameraPosition;
+        float distance = (Time.time - beginTime) * flySpeed;
+        float travelDiv = distance / lengthToTravel;
+        while ((currentLocation.position != DesiredScenePosition.position) && (currentLocation.position.x < DesiredScenePosition.position.x))
+        {
+            sceneCam.transform.position = Vector3.Lerp(currentLocation.position, DesiredScenePosition.position, flySpeed * Time.deltaTime);
+            currentLocation.position = sceneCam.transform.position;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        }
+    }
+
 }
